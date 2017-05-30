7243b98a79d64e9ac6679714b08075510c33cd61
YanlongM <breakxerxes@Xerxes.local>
2017-05-30 21:45:57 +1000

    final

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,7 +3,30 @@ $(document).ready(function() {
 
 // Add jQuery here
 
-
+$('.l').mouseenter(function() {
+  $('.l__image').addClass('showme');
+});
+$('.l').mouseleave(function() {
+  $('.l__image').removeClass('showme');
+});
+$('.ll').mouseenter(function() {
+  $('.ll__image').addClass('showme');
+});
+$('.ll').mouseleave(function() {
+  $('.ll__image').removeClass('showme');
+});
+$('.h').mouseenter(function() {
+  $('.h__image').addClass('showme');
+});
+$('.h').mouseleave(function() {
+  $('.h__image').removeClass('showme');
+});
+$('.ts').mouseenter(function() {
+  $('.ts__image').addClass('showme');
+});
+$('.ts').mouseleave(function() {
+  $('.ts__image').removeClass('showme');
+});
 
 
 
